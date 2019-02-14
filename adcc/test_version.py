8825ddcb18b265af0762e7888f7ad0a573fa0a6a
Michael F. Herbst <info@michael-herbst.com>
2019-02-14 21:31:59 +0100

    Test for version

---
+++ adcc/test_version.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2019 by the adcc authors
+##
+## This file is part of adcc.
+##
+## adcc is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## adcc is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with adcc. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+
+import adcc
+import libadcc
+import unittest
+
+
+class VersionTest(unittest.TestCase):
+    def test_versions(self):
+        """Test that versions of libadcc and adcc agree"""
+        assert adcc.__version__ == libadcc.__version__
