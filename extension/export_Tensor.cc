f15b360775a22f23c14721eadb995efd2872495d
Michael F. Herbst <info@michael-herbst.com>
2019-05-15 09:52:10 +0200

    Constructed tensors are zeroed by default

--- extension/export_Tensor.cc
+++ extension/export_Tensor.cc
@@ -356,7 +356,7 @@ void export_Tensor(py::module& m) {
   py::class_<Tensor, std::shared_ptr<Tensor>>(
         m, "Tensor",
         "Class representing the Tensor objects used for computations in adcman")
-        .def(py::init(&adcc::make_tensor_empty),
+        .def(py::init(&adcc::make_tensor_zero),
              "Construct a Tensor object using a Symmetry object describing its symmetry "
              "properties.\n"
              "The returned object is not guaranteed to contain initialised memory.")
