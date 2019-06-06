bda1b8a517ae3d9ff73c6c75360ff7867f703ad3
Michael F. Herbst <info@michael-herbst.com>
2019-06-06 23:10:05 +0200

    water sto3g generator

---
+++ adcc/testdata/generate_hfdata_h2o_sto3g.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2018 by the adcc authors
+##
+## This file is part of adcc.
+##
+## adcc is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## adcc is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with adcc. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+from pyscf import gto, scf
+
+import adcc.backends.pyscf
+
+from adcc import hdf5io
+
+# Run SCF in pyscf and converge super-tight using an EDIIS
+mol = gto.M(
+    atom='O 0 0 0;'
+         'H 0 0 1.795239827225189;'
+         'H 1.693194615993441 0 -0.599043184453037',
+    basis='sto-3g',
+    unit="Bohr"
+)
+mf = scf.RHF(mol)
+mf.conv_tol = 1e-13
+mf.conv_tol_grad = 1e-12
+mf.diis = scf.EDIIS()
+mf.diis_space = 3
+mf.max_cycle = 500
+mf.kernel()
+
+hfdict = adcc.backends.pyscf.convert_scf_to_dict(mf)
+hfdict["n_core_orbitals"] = 1
+hdf5io.save("h2o_sto3g_hfdata.hdf5", hfdict)
