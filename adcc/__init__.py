5cc6294e3fa62266379f2d96c0f3a2d5d46aeae3
Michael F. Herbst <info@michael-herbst.com>
2019-06-10 23:49:08 +0200

    Bump version: 0.10.4 → 0.11.0

--- adcc/__init__.py
+++ adcc/__init__.py
@@ -61,7 +61,7 @@ __all__ = ["run_adc", "AdcMatrix", "AdcMethod", "Symmetry", "ReferenceState",
            "cvs_adc0", "cvs_adc1", "cvs_adc2", "cvs_adc2x",
            "banner"]
 
-__version__ = "0.10.4"
+__version__ = "0.11.0"
 __license__ = "LGPL v3"
 __authors__ = "Michael F. Herbst and Maximilian Scheurer"
 __email__ = "info@michael-herbst.com"
