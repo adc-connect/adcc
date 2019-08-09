08da1386b7d19a70eb9438830dbb9938d86b6205
Michael F. Herbst <info@michael-herbst.com>
2019-08-09 22:57:54 +0200

    Add some example scripts

---
+++ examples/water/pyscf_ccpvdz_fc_adc3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+from pyscf import gto, scf
+
+import adcc
+
+# Run SCF in pyscf
+mol = gto.M(
+    atom='O 0 0 0;'
+         'H 0 0 1.795239827225189;'
+         'H 1.693194615993441 0 -0.599043184453037',
+    basis='cc-pvdz',
+    unit="Bohr"
+)
+scfres = scf.RHF(mol)
+scfres.conv_tol = 1e-13
+scfres.kernel()
+
+# Run an adc3 calculation:
+singlets = adcc.adc3(scfres, frozen_core=1, n_singlets=3)
+triplets = adcc.adc3(singlets.matrix, n_triplets=3)
+
+print(singlets.describe())
+print()
+print(triplets.describe())
