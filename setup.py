32bf263e01c36b4f53a64697cc3068859d7db988
Maximilian Scheurer <maximilian.scheurer@iwr.uni-heidelberg.de>
2020-03-03 22:52:34 +0100

    Multiple Python versions for conda (#47)
    * build multiple conda versions
    * bump adccore version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except ImportError:
 
 # Version of the python bindings and adcc python package.
 __version__ = "0.14.0"
-adccore_version = ("0.13.6", "")  # (base version, unstable postfix)
+adccore_version = ("0.13.7", "")  # (base version, unstable postfix)
 
 
 def is_conda_build():
