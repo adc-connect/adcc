sign fix

adcc/backends/test_psi4.py
@@ -138,7 +138,7 @@ class TestPsi4(unittest.TestCase):
         operator_import_test(wfn, ao_dip, operator="electric_dipole")
 
         # Test magnetic dipole
-        ao_dip = [np.array(comp) for comp in mints.ao_angular_momentum()]
+        ao_dip = [-1.0 * np.array(comp) for comp in mints.ao_angular_momentum()]
         operator_import_test(wfn, ao_dip, operator="magnetic_dipole")
 
         # Test linear momentum