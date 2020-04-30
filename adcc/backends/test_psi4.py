fix signs

adcc/backends/test_psi4.py
@@ -111,12 +111,12 @@ class TestPsi4(unittest.TestCase):
 
         # Test dipole
         mints = psi4.core.MintsHelper(wfn.basisset())
-        ao_dip = [np.array(comp) for comp in mints.ao_dipole()]
+        ao_dip = [-1.0 * np.array(comp) for comp in mints.ao_dipole()]
         operator_import_test(wfn, ao_dip)
 
         # TODO: generalize, code block repeated below...
         # Test magnetic dipole
-        ao_dip = [np.array(comp) for comp in mints.ao_angular_momentum()]
+        ao_dip = [-1.0 * np.array(comp) for comp in mints.ao_angular_momentum()]
         operator_import_test(wfn, ao_dip, operator="magnetic_dipole")
 
         # Test linear momentum
@@ -134,7 +134,7 @@ class TestPsi4(unittest.TestCase):
 
         # Test dipole
         mints = psi4.core.MintsHelper(wfn.basisset())
-        ao_dip = [np.array(comp) for comp in mints.ao_dipole()]
+        ao_dip = [-1.0 * np.array(comp) for comp in mints.ao_dipole()]
         operator_import_test(wfn, ao_dip, operator="electric_dipole")
 
         # Test magnetic dipole

adcc/backends/testing.py
@@ -23,7 +23,6 @@
 import adcc
 import numpy as np
 
-from ..misc import assert_allclose_signfix
 from collections import namedtuple
 
 ii, jj, kk, ll = 0, 1, 2, 3
@@ -179,7 +178,7 @@ def operator_import_test(scfres, ao_dict, operator="electric_dipole"):
             dip_mock["v1o1"] = dip_vo
 
         for b in dip_imported_comp.blocks:
-            assert_allclose_signfix(
+            np.testing.assert_allclose(
                 dip_mock[b], dip_imported_comp[b].to_ndarray(),
                 atol=refstate.conv_tol
             )