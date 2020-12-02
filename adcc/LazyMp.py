2348013a1e33e5cf37e32d499a8c69b64ff3be2f
Maximilian Scheurer <max.scheurer@me.com>
2020-12-02 14:12:12 +0100

    py-side mp

--- adcc/LazyMp.py
+++ adcc/LazyMp.py
@@ -24,9 +24,22 @@ import libadcc
 import numpy as np
 
 from .mp import compute_mp2_diffdm  # noqa: F401
+from .functions import direct_sum, evaluate, einsum
 from .misc import cached_property
 from .ReferenceState import ReferenceState
 from .OneParticleOperator import OneParticleOperator, product_trace
+from .timings import Timer
+from . import block as b
+
+
+def compute_t2(eri, df1, df2):
+    return evaluate(
+        eri / direct_sum("ia+jb->ijab", df1, df2).symmetrise((2, 3))
+    )
+
+
+def compute_energy_correction(eri, t2):
+    return -0.25 * einsum('ijab,ijab->', eri, t2)
 
 
 class LazyMp:
@@ -40,17 +53,14 @@ class LazyMp:
             raise TypeError("hf needs to be a ReferenceState "
                             "or a HartreeFockSolution_i")
         self.reference_state = hf
-        self.__cpp = libadcc.LazyMp(hf)
         self.mospaces = hf.mospaces
+        self.timer = Timer()
         self.has_core_occupied_space = hf.has_core_occupied_space
-
-    @property
-    def timer(self):
-        return self.__cpp.timer
+        # FIXME: just a hack to let tests pass
+        self.t2_set = {}
+        # TODO: implement cache
 
     def __getattr__(self, attr):
-        from . import block as b
-
         # Shortcut some quantities, which are needed most often
         if attr.startswith("t2") and len(attr) == 4:  # t2oo, t2oc, t2cc
             xxvv = b.__getattr__(attr[2:4] + "vv")
@@ -60,13 +70,32 @@ class LazyMp:
 
     def df(self, space):
         """Delta Fock matrix"""
-        # TODO This is so cheap to compute that it does not really deserve
-        #      any caching
-        return self.__cpp.df(space)
+        hf = self.reference_state
+        s1 = space[:2]
+        s2 = space[2:]
+        fC = hf.fock(s1 + s1).diagonal()
+        fv = hf.fock(s2 + s2).diagonal()
+        return direct_sum("-i+a->ia", fC, fv).evaluate()
 
     def t2(self, space):
         """T2 amplitudes"""
-        return self.__cpp.t2(space)
+        # FIXME: just a hack to let tests pass
+        if len(self.t2_set):
+            return self.t2_set[space]
+        hf = self.reference_state
+        if space == b.oovv:
+            eia = self.df(b.ov)
+            return compute_t2(hf.oovv, eia, eia)
+        elif space == b.ocvv:
+            eia = self.df(b.ov)
+            eca = self.df(b.cv)
+            return compute_t2(hf.ocvv, eia, eca)
+        elif space == b.ccvv:
+            eca = self.df(b.cv)
+            return compute_t2(hf.ccvv, eca, eca)
+        else:
+            raise NotImplementedError("T2 amplitudes not "
+                                      f"implemented for space {space}.")
 
     def set_t2(self, space, tensor):
         """
@@ -74,25 +103,54 @@ class LazyMp:
         amplitudes in this class. Note, that potential other caches, such as
         computed ADC intermediates are *not* automatically invalidated.
         """
-        return self.__cpp.set_t2(space, tensor)
+        # FIXME: just a temporary hack
+        self.t2_set[space] = tensor
 
     def td2(self, space):
         """Return the T^D_2 term"""
-        return self.__cpp.td2(space)
+        if space != b.oovv:
+            raise NotImplementedError()
+        t2erit = self.t2eri(b.oovv, b.ov).transpose((1, 0, 2, 3))
+        denom = direct_sum(
+            'ia,jb->ijab', self.df(b.ov), self.df(b.ov)
+        ).symmetrise(0, 1)
+        return evaluate(
+            (+ 4.0 * t2erit.antisymmetrise(2, 3).antisymmetrise(0, 1)
+             - 0.5 * self.t2eri(b.oovv, b.vv)
+             - 0.5 * self.t2eri(b.oovv,  b.oo)) / denom
+        )
 
     def t2eri(self, space, contraction):
         """
         Return the T2 tensor with ERI tensor contraction intermediates.
         These are called pi1 to pi7 in libadc.
         """
-        return self.__cpp.t2eri(space, contraction)
+        hf = self.reference_state
+        expressions = {
+            # space + contraction
+            b.ooov + b.vv: einsum('ijbc,kabc->ijka', self.t2oo, hf.ovvv),
+            b.ooov + b.ov: einsum('ilab,lkjb->ijka', self.t2oo, hf.ooov),
+            b.oovv + b.oo: einsum('klab,ijkl->ijab', self.t2oo, hf.oooo),
+            b.oovv + b.ov: einsum('jkac,kbic->ijab', self.t2oo, hf.ovov),
+            b.oovv + b.vv: einsum('ijcd,abcd->ijab', self.t2oo, hf.vvvv),
+            b.ovvv + b.oo: einsum('jkbc,jkia->iabc', self.t2oo, hf.ooov),
+            b.ovvv + b.ov: einsum('ijbd,jcad->iabc', self.t2oo, hf.ovvv),
+        }
+        return expressions[space + contraction].evaluate()
 
     @property
     def mp2_diffdm(self):
         """
         Return the MP2 differensce density in the MO basis.
         """
-        ret = OneParticleOperator.from_cpp(self.__cpp.mp2_diffdm)
+        hf = self.reference_state
+        ret = OneParticleOperator(self.mospaces, is_symmetric=True)
+        blocks = ["oo", "ov", "vv"]
+        if self.has_core_occupied_space:
+            blocks += ["cc", "co", "cv"]
+        for bl in blocks:
+            ba = getattr(b, bl)
+            ret[ba] = compute_mp2_diffdm(hf, self, bl, apply_cvs=False)
         ret.reference_state = self.reference_state
         return ret
 
@@ -124,7 +182,21 @@ class LazyMp:
 
     def energy_correction(self, level=2):
         """Obtain the MP energy correction at a particular level"""
-        return self.__cpp.energy_correction(level)
+        if level > 3:
+            raise NotImplementedError(f"MP({level}) energy correction "
+                                      "not implemented.")
+        if level < 2:
+            return 0.0
+        hf = self.reference_state
+        if level == 2 and not self.has_core_occupied_space:
+            return compute_energy_correction(hf.oovv, self.t2oo)
+        elif level == 2 and self.has_core_occupied_space:
+            pairs = [(self.t2oo, hf.oovv),
+                     (2.0 * self.t2oc, hf.ocvv),
+                     (self.t2cc, hf.ccvv)]
+            return sum(compute_energy_correction(*p) for p in pairs)
+        elif level == 3:
+            return compute_energy_correction(hf.oovv, self.td2(b.oovv))
 
     def energy(self, level=2):
         """
--- adcc/mp.py
+++ adcc/mp.py
@@ -31,7 +31,7 @@ from .Intermediates import register_as_intermediate
 def compute_mp2_diffdm(hf, mp, space, apply_cvs=None):
     """Expressions to compute the MP2 difference density matrix
 
-    If `apply_cvs` is `False` than the CVS approximation will not be
+    If `apply_cvs` is `False` then the CVS approximation will not be
     applied during the density matrix computation even if it is
     applied in the hf reference. This is done in order to be
     able to obtain the *full* MP2 difference density (needed for
@@ -40,22 +40,64 @@ def compute_mp2_diffdm(hf, mp, space, apply_cvs=None):
     """
     if apply_cvs is None:
         apply_cvs = hf.has_core_occupied_space
-    elif apply_cvs is False:
-        raise NotImplementedError("Not applying the CVS if reference has "
-                                  "it is not yet implemented")
-    assert not hf.has_core_occupied_space or apply_cvs
+    # elif apply_cvs is False:
+    #     raise NotImplementedError("Not applying the CVS if reference has "
+    #                               "it is not yet implemented")
+    # assert not hf.has_core_occupied_space or apply_cvs
 
-    t2 = mp.t2(b.oovv)
-    if space == "oo":
-        return -0.5 * einsum("ikab,jkab->ij", t2, t2)
-    elif space == "ov":
-        df = direct_sum("a-i->ia", hf.fvv.diagonal(), hf.foo.diagonal())
-        return -0.5 * (+ einsum("ijbc,jabc->ia", t2, hf.ovvv)
-                       + einsum("jkib,jkab->ia", hf.ooov, t2)) / df
-    elif space == "vv":
-        return 0.5 * einsum("ijac,ijbc->ab", t2, t2)
+    # TODO: too much if/else..
+    if apply_cvs or not hf.has_core_occupied_space:
+        t2 = mp.t2(b.oovv)
+        if space == "oo":
+            return -0.5 * einsum("ikab,jkab->ij", t2, t2)
+        elif space == "ov":
+            df = direct_sum("a-i->ia", hf.fvv.diagonal(), hf.foo.diagonal())
+            return -0.5 * (+ einsum("ijbc,jabc->ia", t2, hf.ovvv)
+                           + einsum("jkib,jkab->ia", hf.ooov, t2)) / df
+        elif space == "vv":
+            return 0.5 * einsum("ijac,ijbc->ab", t2, t2)
+        else:
+            raise NotImplementedError(f"Space {space} not implemented.")
     else:
-        raise NotImplementedError(f"Space {space} not implemented.")
+        if space == "cc":
+            return -0.5 * (+ einsum("kIab,kJab->IJ", mp.t2oc, mp.t2oc)
+                           + einsum('LIab,LJab->IJ', mp.t2cc, mp.t2cc))
+        elif space == "co":
+            return -0.5 * (+ einsum("kIab,kjab->Ij", mp.t2oc, mp.t2oo)
+                           + einsum("ILab,jLab->Ij", mp.t2cc, mp.t2oc))
+        elif space == "cv":
+            return -0.5 * (
+                - 1.0 * einsum("jIbc,jabc->Ia", mp.t2oc, hf.ovvv)
+                + 1.0 * einsum("jkIb,jkab->Ia", hf.oocv, mp.t2oo)
+                + 1.0 * einsum("jMIb,jMab->Ia", hf.occv, mp.t2oc)
+                + 1.0 * einsum("ILbc,Labc->Ia", mp.t2cc, hf.cvvv)
+                + 1.0 * einsum("kLIb,kLab->Ia", hf.occv, mp.t2oc)
+                + 1.0 * einsum("LMIb,LMab->Ia", hf.cccv, mp.t2cc)
+            ) / mp.df(b.cv)
+        elif space == "oc":
+            return -0.5 * (+ einsum("kiab,kJab->iJ", mp.t2oo, mp.t2oc)
+                           + einsum("iLab,JLab->iJ", mp.t2oc, mp.t2cc))
+        elif space == "oo":
+            return -0.5 * (+ einsum("ikab,jkab->ij", mp.t2oo, mp.t2oo)
+                           + einsum("iLab,jLab->ij", mp.t2oc, mp.t2oc))
+        elif space == "ov":
+            return -0.5 * (
+                + einsum("ijbc,jabc->ia", mp.t2oo, hf.ovvv)
+                + einsum("jkib,jkab->ia", hf.ooov, mp.t2oo)
+                + einsum("jMib,jMab->ia", hf.ocov, mp.t2oc)
+                + einsum("iLbc,Labc->ia", mp.t2oc, hf.cvvv)
+                + einsum("kLib,kLab->ia", hf.ocov, mp.t2oc)
+                + einsum("iMLb,LMab->ia", hf.occv, mp.t2cc)
+                - einsum("iLMb,LMab->ia", hf.occv, mp.t2cc)
+            ) / mp.df(b.ov)
+        elif space == "vv":
+            return (
+                + 0.5 * einsum("klac,klbc->ab", mp.t2oo, mp.t2oo)
+                + 0.5 * einsum("IJac,IJbc->ab", mp.t2cc, mp.t2cc)
+                + 1.0 * einsum("kJac,kJbc->ab", mp.t2oc, mp.t2oc)
+            )
+        else:
+            raise NotImplementedError(f"Space {space} not implemented.")
 
 
 #
