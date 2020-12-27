fee393e22b2340d04a5923982005f42acab63fab
Maximilian Scheurer <max.scheurer@me.com>
2020-12-27 15:46:30 +0100

    nuke cpp operator, py-side implementation

--- adcc/OneParticleOperator.py
+++ adcc/OneParticleOperator.py
@@ -20,12 +20,17 @@
 ## along with adcc. If not, see <http://www.gnu.org/licenses/>.
 ##
 ## ---------------------------------------------------------------------
+import numpy as np
+from itertools import product, combinations_with_replacement
+
 import libadcc
 
-from .functions import evaluate
+from .functions import evaluate, einsum
+from .MoSpaces import split_spaces
+from .Tensor import Tensor
 
 
-class OneParticleOperator(libadcc.OneParticleOperator):
+class OneParticleOperator:
     def __init__(self, spaces, is_symmetric=True):
         """
         Construct an OneParticleOperator object. All blocks are initialised
@@ -33,7 +38,6 @@ class OneParticleOperator(libadcc.OneParticleOperator):
 
         Parameters
         ----------
-
         spaces : adcc.Mospaces or adcc.ReferenceState or adcc.LazyMp
             MoSpaces object
 
@@ -41,9 +45,10 @@ class OneParticleOperator(libadcc.OneParticleOperator):
             Is the operator symmetric?
         """
         if hasattr(spaces, "mospaces"):
-            super().__init__(spaces.mospaces, is_symmetric, "1")
+            self.mospaces = spaces.mospaces
         else:
-            super().__init__(spaces, is_symmetric, "1")
+            self.mospaces = spaces
+        self.is_symmetric = is_symmetric
 
         # Set reference_state if possible
         if isinstance(spaces, libadcc.ReferenceState):
@@ -52,41 +57,161 @@ class OneParticleOperator(libadcc.OneParticleOperator):
             assert isinstance(spaces.reference_state, libadcc.ReferenceState)
             self.reference_state = spaces.reference_state
 
-    @classmethod
-    def from_cpp(cls, cpp_operator):
-        assert cpp_operator.cartesian_transform == "1"
-        ret = cls(cpp_operator.mospaces, cpp_operator.is_symmetric)
-        for b in cpp_operator.blocks_nonzero:
-            ret.set_block(b, cpp_operator.block(b))
+        occs = sorted(self.mospaces.subspaces_occupied, reverse=True)
+        virts = sorted(self.mospaces.subspaces_virtual, reverse=True)
+        self.orbital_subspaces = occs + virts
+        if self.is_symmetric:
+            combs = list(
+                combinations_with_replacement(self.orbital_subspaces, r=2)
+            )
+        else:
+            combs = list(product(self.orbital_subspaces, repeat=2))
+        self.blocks = ["".join(com) for com in combs]
+        self._tensors = {}
+
+    @property
+    def shape(self):
+        """
+        Returns the shape tuple of the OneParticleOperator
+        """
+        size = sum(self.mospaces.n_orbs(ss) for ss in self.orbital_subspaces)
+        return (size, size)
+
+    @property
+    def size(self):
+        """
+        Returns the number of elements of the OneParticleOperator
+        """
+        sh = self.shape
+        return sh[0] * sh[1]
+
+    @property
+    def blocks_nonzero(self):
+        """
+        Returns a list of the non-zero block labels
+        """
+        return [b for b in self.blocks if b in self._tensors]
+
+    def is_zero_block(self, block):
+        """
+        Checks if block is a zero block
+        """
+        return block not in self.blocks_nonzero
+
+    def block(self, block):
+        """
+        Returns tensor of the given block
+        """
+        # TODO: sanity checks
+        assert block in self.blocks_nonzero
+        return self._tensors[block]
+
+    def __getitem__(self, block):
+        assert block in self.blocks
+        s1, s2 = split_spaces(block)
+        if block not in self._tensors:
+            sym = libadcc.make_symmetry_operator(
+                self.mospaces, block, self.is_symmetric, "1"
+            )
+            self._tensors[block] = Tensor(sym)
+        return self._tensors[block]
+
+    def __setitem__(self, block, tensor):
+        self.set_block(block, tensor)
+
+    def set_block(self, block, tensor):
+        """
+        Assigns tensor to a given block
+        """
+        assert block in self.blocks
+        s1, s2 = split_spaces(block)
+        self._tensors[block] = tensor
+
+    def set_zero_block(self, block):
+        """
+        Set a given block as zero block
+        """
+        assert block in self.blocks
+        self._tensors.pop(block)
+
+    def to_ndarray(self):
+        """
+        Returns the OneParticleOperator as a contiguous
+        np.ndarray instance with all blocks
+        """
+        ret = np.zeros((self.shape))
+        row_offset = 0
+        for i, sp1 in enumerate(self.orbital_subspaces):
+            n_orbs1 = self.mospaces.n_orbs(sp1)
+            col_offset = 0
+            for j, sp2 in enumerate(self.orbital_subspaces):
+                n_orbs2 = self.mospaces.n_orbs(sp2)
+                if self.is_symmetric and j < i:
+                    if not self.is_zero_block(sp2 + sp1):
+                        dm_12 = self.block(sp2 + sp1)
+                        ret[row_offset:row_offset + n_orbs1,
+                            col_offset:col_offset + n_orbs2] = dm_12.to_ndarray().T
+                else:
+                    if not self.is_zero_block(sp1 + sp2):
+                        dm_12 = self.block(sp1 + sp2)
+                        ret[row_offset:row_offset + n_orbs1,
+                            col_offset:col_offset + n_orbs2] = dm_12.to_ndarray()
+                col_offset += n_orbs2
+            row_offset += n_orbs1
         return ret
 
     def copy(self):
         """
         Return a deep copy of the OneParticleOperator
         """
-        ret = OneParticleOperator.from_cpp(super().copy())
+        ret = OneParticleOperator(self.mospaces, self.is_symmetric)
+        for b in self.blocks_nonzero:
+            ret.set_block(b, self.block(b).copy())
         if hasattr(self, "reference_state"):
             ret.reference_state = self.reference_state
         return ret
+
+    def __transform_to_ao(self, refstate_or_coefficients):
+        if isinstance(refstate_or_coefficients, libadcc.ReferenceState):
+            hf = refstate_or_coefficients
+            coeff_map = {}
+            for sp in self.orbital_subspaces:
+                coeff_map[sp + "_a"] = hf.orbital_coefficients_alpha(sp + "b")
+                coeff_map[sp + "_b"] = hf.orbital_coefficients_beta(sp + "b")
+        else:
+            coeff_map = refstate_or_coefficients
+
+        dm_bb_a = 0
+        dm_bb_b = 0
+        for block in self.blocks_nonzero:
+            s1, s2 = split_spaces(block)
+            pref = 2.0 if (s1 != s2 and self.is_symmetric) else 1.0
+            # TODO: sanity check
+            dm_bb_a += pref * einsum("ip,ij,jq->pq", coeff_map[f"{s1}_a"],
+                                     self[block], coeff_map[f"{s2}_a"])
+            dm_bb_b += pref * einsum("ip,ij,jq->pq", coeff_map[f"{s1}_b"],
+                                     self[block], coeff_map[f"{s2}_b"])
+        if self.is_symmetric:
+            dm_bb_a = dm_bb_a.symmetrise()
+            dm_bb_b = dm_bb_b.symmetrise()
+        return (dm_bb_a.evaluate(), dm_bb_b.evaluate())
 
     def to_ao_basis(self, refstate_or_coefficients=None):
         """
         TODO DOCME
         """
         if isinstance(refstate_or_coefficients, (dict, libadcc.ReferenceState)):
-            return super().to_ao_basis(refstate_or_coefficients)
+            return self.__transform_to_ao(refstate_or_coefficients)
         elif refstate_or_coefficients is None:
             if not hasattr(self, "reference_state"):
                 raise ValueError("Argument reference_state is required if no "
                                  "reference_state is stored in the "
                                  "OneParticleOperator")
-            return super().to_ao_basis(self.reference_state)
+            return self.__transform_to_ao(self.reference_state)
         else:
             raise TypeError("Argument type not supported.")
 
     def __iadd__(self, other):
-        if not isinstance(other, libadcc.OneParticleOperator):
-            return NotImplemented
         if self.mospaces != other.mospaces:
             raise ValueError("Cannot add OneParticleOperators with "
                              "differing mospaces.")
@@ -119,8 +244,6 @@ class OneParticleOperator(libadcc.OneParticleOperator):
         return self
 
     def __isub__(self, other):
-        if not isinstance(other, libadcc.OneParticleOperator):
-            return NotImplemented
         if self.mospaces != other.mospaces:
             raise ValueError("Cannot subtract OneParticleOperators with "
                              "differing mospaces.")
