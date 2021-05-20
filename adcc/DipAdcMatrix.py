7c6bbcb45c42150a6222cdd2c7a12ddbda4e5953
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2021-05-20 10:31:20 +0200

    Add license to DipAdcMatrix

--- adcc/DipAdcMatrix.py
+++ adcc/DipAdcMatrix.py
@@ -1,4 +1,26 @@
-from .AdcMatrix import AdcMatrix
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2020 by the adcc authors
+##
+## This file is part of adcc.
+##
+## adcc is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## adcc is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with adcc. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+ffrom .AdcMatrix import AdcMatrix
 from .adc_dip import matrix as dipmatrix
 
 class DipAdcMatrix(AdcMatrix):
