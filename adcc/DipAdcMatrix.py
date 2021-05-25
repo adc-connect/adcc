ae8a373e0a3305571d2d05482c62630c0396713e
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2021-05-25 14:50:13 +0200

    Translate matrix computations from libadc/adc_dip

--- adcc/DipAdcMatrix.py
+++ adcc/DipAdcMatrix.py
@@ -25,10 +25,11 @@ from .adc_dip import matrix as dipmatrix
 
 class DipAdcMatrix(AdcMatrix):
     default_block_orders = {
-        "adc0":  dict(hh=0, hh_ph=None, hh_ph=None, hhhh_ph=None),  # noqa: E501
-        "adc1":  dict(hh=1, hh_ph=None, hh_ph=None, hhhh_ph=None),  # noqa: E501
-        "adc2":  dict(hh=2, hh_ph=1,    hh_ph=1,    hhhh_ph=0),     # noqa: E501
+        "adc0":  dict(hh_hh=0, phhh_hh=None, hh_phhh=None, phhh_phhh=None),  # noqa: E501
+        "adc1":  dict(hh_hh=1, phhh_hh=None, hh_phhh=None, phhh_phhh=None),  # noqa: E501
+        "adc2":  dict(hh_hh=2, phhh_hh=1,    hh_phhh=1,    phhh_phhh=0),     # noqa: E501
     }
 
     def __generate_matrix_block(self, *args, **kwargs):
         return dipmatrix.block(*args, **kwargs)
+
