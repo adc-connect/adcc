e4ca2edf77884474bb685122efc272a900151509
Michael F. Herbst <info@michael-herbst.com>
2021-06-02 17:59:17 +0200

    Bump version: 0.15.8 → 0.15.9

--- setup.py
+++ setup.py
@@ -508,7 +508,7 @@ adccsetup(
         "Issues": "https://github.com/adc-connect/adcc/issues",
     },
     #
-    version="0.15.8",
+    version="0.15.9",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
