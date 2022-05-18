046c48794c2ed155f0480a3b4585c50bc8be445d
Michael F. Herbst <info@michael-herbst.com>
2022-05-18 09:40:26 +0200

    Bump version: 0.15.13 → 0.15.14

--- adcc/__init__.py
+++ adcc/__init__.py
@@ -66,7 +66,7 @@ __all__ = ["run_adc", "InputError", "AdcMatrix", "AdcBlockView",
            "cvs_adc0", "cvs_adc1", "cvs_adc2", "cvs_adc2x", "cvs_adc3",
            "banner"]
 
-__version__ = "0.15.13"
+__version__ = "0.15.14"
 __license__ = "GPL v3"
 __url__ = "https://adc-connect.org"
 __authors__ = ["Michael F. Herbst", "Maximilian Scheurer", "Jonas Leitner"]
--- setup.py
+++ setup.py
@@ -509,7 +509,7 @@ adccsetup(
         "Issues": "https://github.com/adc-connect/adcc/issues",
     },
     #
-    version="0.15.13",
+    version="0.15.14",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
