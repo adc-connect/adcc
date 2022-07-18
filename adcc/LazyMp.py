eefdf932359332476bad315a82dcf225a3d65e1b
Rouven Maier <rmaier@ccserv1.iwr.uni-heidelberg.de>
2022-07-18 15:10:02 +0200

    added 3rd order amplitudes up to doubles and implemented raw version of third order tdm and s2s. Second order triples amplitude will be added soon.#

--- adcc/LazyMp.py
+++ adcc/LazyMp.py
@@ -92,6 +92,75 @@ class LazyMp:
             - 0.5 * self.t2eri(b.oovv, b.vv)
             - 0.5 * self.t2eri(b.oovv, b.oo)
         ) / denom
+    
+    @cached_member_function
+    def tt2(self, space):
+        """Second prder triple amplitudes"""
+        t2 = self.t2(b.oovv)
+        amp = (
+            
+            )
+
+    @cached_member_function
+    def tq2(self, space):
+        """Second order quadruple amplitudes"""
+        t2 = self.t2(b.oovv)
+        amp = (einsum('ijab,klcd->ijabklcd', t2, t2)
+            - einsum('ijac,klbd->ijacklbd', t2, t2)
+            + einsum('ijad,klbc->ijadklbc', t2, t2)
+            + einsum('ijbc,klad->ijbcklad', t2, t2)
+            - einsum('ijbd,klac->ijbdklac', t2, t2)
+            + einsum('ijcd,klab->ijcdklab', t2, t2)
+            - einsum('ikab,jlcd->ikabjlcd', t2, t2)
+            + einsum('ikac,jlbd->ikacjlbd', t2, t2)
+            - einsum('ikad,jlbc->ikadjlbc', t2, t2)
+            - einsum('ikbc,jlad->ikbcjlad', t2, t2)
+            + einsum('ikbd,jlac->ikbdjlac', t2, t2)
+            - einsum('ikcd,jlab->ikcdjlab', t2, t2)
+            + einsum('ilab,jkcd->ilabjkcd', t2, t2)
+            - einsum('ilac,jkbd->ilacjkbd', t2, t2)
+            + einsum('ilad,jkbc->iladjkbc', t2, t2)
+            + einsum('ilbc,jkad->ilbcjkad', t2, t2)
+            - einsum('ilbd,jkac->ilbdjkac', t2, t2)
+            + einsum('ilcd,jkab->ilcdjkab', t2, t2))
+        return amp
+    
+    @cached_member_function
+    def ts3(self, space):
+        """Third order single amplitudes"""
+        hf = self.reference_state
+        p0 = self.mp2_diffdm #= t_{ia}^{(2)}
+        td2 = self.td2(b.oovv)
+        #tt2 = self.tt2(b.ooovvv) 
+        
+        denom = - self.df(b.ov)
+        amp = (- einsum('jaib,jb->ia', hf.ovov, p0.ov)
+            + 0.5 * einsum('jkib,jkab->ia', hf.ooov, td2)
+            + 0.5 * einsum('jabc,ijbc->ia', hf.ovvv, td2))
+               # + 0.25 * einsum('jkbc,ijkabc->ia', hf.oovv, tt2)
+        return amp/denom
+    
+    @cached_member_function
+    def td3(self, space):
+        """Third order double amplitudes"""
+        hf = self.reference_state
+        p0 = self.mp2_diffdm
+        t2 = self.t2(b.oovv)
+        td2 = self.td2(b.oovv)
+        #tt2 = self.tt2(b.ooovvv)
+        #tq2 = self.tq2(b.oooovvvv)
+
+        denom = direct_sum('ia,jb->ijab',self.df(b.ov),self.df(b.ov))
+        amp = (2 * einsum('jc,abic->ijab', p0, hf.vvov).antisymmetrise(0,1)
+            + 2 * einsum('kb,kaij->ijab', p0, hf.ovoo).antisymmetrise(2,3)
+            + 4 * einsum('jkac,kbjc->ijab', td2, hf.ovov).antisymmetrise(0,1).antisymmetrise(2,3)
+            - 0.5 * einsum('ijcd,abcd->ijab', td2, hf.vvvv)
+            - 0.5 * einsum('klab,klij->ijab', td2, hf.oooo)
+            #+ einsum('jklabc,klic->ijab', tt2, hf.ooov).antisymmetrise(0,1)
+            #+ einsum('ijkbcd,kacd->ijab', tt2, hf.ovvv).antisymmetrise(2,3)
+            #+ 0.25 * einsum('ijklabcd,klcd->ijab', tq2, hf.oovv)
+            - 0.25 * einsum('ijab,klcd,klcd->ijab', t2, t2, hf.oovv))
+        return amp/denom
 
     @cached_member_function
     def t2eri(self, space, contraction):
