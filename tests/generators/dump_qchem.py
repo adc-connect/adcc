d0029667ab34bba4ba396fb448d08f005537831b
jonasleitner <jonas.leitner1@gmx.de>
2024-11-26 08:07:57 +0100

    add dump functionality for qchem data

---
+++ tests/generators/dump_qchem.py
@@ -0,0 +1,60 @@
+from import_qchem_data import _mp_data, _excited_state_data
+import test_cases
+
+from adcc.AdcMethod import AdcMethod
+from adcc.hdf5io import emplace_dict
+
+from collections import defaultdict
+from pathlib import Path
+import h5py
+
+_testdata_dirname = "data"
+
+
+def dump_groundstate(test_case: test_cases.TestCase, gs_data: dict) -> None:
+    """
+    Dump the given ground state data to file. This function assumes that
+    the ground state data has been import with
+    "import_qchem_data.import_groundstate",
+    i.e., that the data is already imported using the correct keys.
+    """
+    assert all(key in _mp_data.values() for key in gs_data)
+    datadir = Path(__file__).parent.parent / _testdata_dirname
+    fname = datadir / f"{test_case.file_name}_adcman_mpdata.hdf5"
+    emplace_dict(
+        gs_data, h5py.File(fname, "w"), compression="gzip", compression_opts=8
+    )
+
+
+def dump_excited_states(test_case: test_cases.TestCase, method: AdcMethod,
+                        state_data: dict) -> None:
+    """
+    Dump the excited states data (excitation energies, amplitude vectors, ...)
+    to a HDF5 file.
+    This function assumes that the data has been imported with
+    "import_qchem_data.import_excited_states", i.e.,
+    state data should be organized as
+    {state_kind: {state_n: data}},
+    where data is itself a dict that already uses the correct keys to store
+    the data. Therefore, this function only collects the data from the
+    different states in a list before dumping.
+    """
+    valid_keys = tuple(
+        key for treemap in _excited_state_data.values()
+        for key in treemap.values()
+    )
+    datadir = Path(__file__).parent.parent / _testdata_dirname
+    fname = datadir / f"{test_case.file_name}_adcman_{method.name}.hdf5"
+    hdf5_file = h5py.File(fname, "w")
+    for kind, kind_data in state_data.items():
+        data_to_dump = defaultdict(list)
+        # sort the kind data to start with the lowest excited state
+        # this way we can just append the values to the corresponding list
+        for _, state_data in sorted(kind_data.items()):
+            assert all(key in valid_keys for key in state_data)
+            for key, value in state_data.items():
+                data_to_dump[key].append(value)
+        kind_group = hdf5_file.create_group(kind)
+        emplace_dict(
+            data_to_dump, kind_group, compression="gzip", compression_opts=8
+        )
