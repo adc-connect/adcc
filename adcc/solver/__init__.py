a588467ffe0013490901461d775f838850d6ab2a
jonasleitner <jonas.leitner1@gmx.de>
2024-12-19 07:13:45 +0100

    add adrians diis solver and implement sigma4+ density

--- adcc/solver/__init__.py
+++ adcc/solver/__init__.py
@@ -2,6 +2,7 @@ from . import davidson
 from .SolverStateBase import EigenSolverStateBase
 from .explicit_symmetrisation import (IndexSpinSymmetrisation,
                                       IndexSymmetrisation)
+from .fixed_point_diis import diis
 
 __all__ = ["IndexSymmetrisation", "IndexSpinSymmetrisation",
-           "davidson", "EigenSolverStateBase"]
+           "davidson", "EigenSolverStateBase", "diis"]
