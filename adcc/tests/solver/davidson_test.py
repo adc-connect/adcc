ffae5d2aeb5f3f8e93022a2a7c1b97839c22180b
Jonas Leitner <jonas.leitner1@gmx.de>
2025-03-06 10:13:21 +0100

    add more davidson tests

--- adcc/tests/solver/davidson_test.py
+++ adcc/tests/solver/davidson_test.py
@@ -25,24 +25,85 @@ import unittest
 import pytest
 
 from adcc import LazyMp
-from adcc.solver.davidson import jacobi_davidson
+from adcc.solver.davidson import jacobi_davidson, eigsh
+from adcc.misc import cached_property
 
 from ..testdata_cache import testdata_cache
 
 
 class TestSolverDavidson(unittest.TestCase):
+    @cached_property
+    def matrix(self):
+        return adcc.AdcMatrix(
+            "adc2", LazyMp(testdata_cache.refstate("h2o_sto3g", case="gen"))
+        )
+
+    def test_n_guesses(self):
+        # we have to have a guess for each state
+        guesses = adcc.guesses_singlet(self.matrix, n_guesses=1, block="ph")
+        with pytest.raises(ValueError):
+            eigsh(self.matrix, guesses, n_ep=2)
+        res = eigsh(self.matrix, guesses, n_ep=1)
+        assert len(res.eigenvalues) == 1
+        # construct 1 state for each guess
+        res = eigsh(self.matrix, guesses)
+        assert len(res.eigenvalues) == 1
+
+    def test_n_block(self):
+        # has to be: n_ep <= n_block <= n_guesses
+        guesses = adcc.guesses_singlet(self.matrix, n_guesses=2, block="ph")
+        with pytest.raises(ValueError):
+            eigsh(self.matrix, guesses, n_ep=2, n_block=1)
+        with pytest.raises(ValueError):
+            eigsh(self.matrix, guesses, n_ep=2, n_block=3)
+        res = eigsh(self.matrix, guesses, n_ep=2, n_block=2)
+        assert len(res.eigenvalues) == 2
+
+    def test_max_subspace(self):
+        # max_subspace >= 2 * n_block
+        guesses = adcc.guesses_singlet(self.matrix, n_guesses=3, block="ph")
+        with pytest.raises(ValueError):
+            eigsh(self.matrix, guesses, n_ep=1, n_block=2, max_subspace=3)
+        res = eigsh(self.matrix, guesses, n_ep=1, n_block=2, max_subspace=4)
+        assert len(res.eigenvalues) == 1
+        # max_subspace >= n_guesses
+        with pytest.raises(ValueError):
+            eigsh(self.matrix, guesses, n_ep=1, n_block=1, max_subspace=2)
+        res = eigsh(self.matrix, guesses, n_ep=1, n_block=1, max_subspace=3)
+        assert len(res.eigenvalues) == 1
+
+    def test_adc0_singlet(self):
+        # ensure that a diagonal matrix (with guesses from the diagonal)
+        # converges in a single iteration to the correct eigenvalues
+        refdata = testdata_cache.adcman_data(
+            system="h2o_sto3g", method="adc0", case="gen"
+        )["singlet"]
+
+        matrix = adcc.AdcMatrix(
+            "adc0", LazyMp(testdata_cache.refstate("h2o_sto3g", case="gen"))
+        )
+
+        # Solve for singlets
+        guesses = adcc.guesses_singlet(matrix, n_guesses=2, block="ph")
+        res = jacobi_davidson(matrix, guesses, n_ep=2)
+
+        assert res.converged
+        assert res.n_iter == 1
+        assert res.n_applies == 2
+
+        ref_singlets = refdata["eigenvalues"]
+        n_states = min(len(ref_singlets), len(res.eigenvalues))
+        assert n_states > 1
+        assert res.eigenvalues[:n_states] == pytest.approx(ref_singlets[:n_states])
+
     def test_adc2_singlets(self):
         refdata = testdata_cache.adcman_data(
             system="h2o_sto3g", method="adc2", case="gen"
         )["singlet"]
 
-        matrix = adcc.AdcMatrix(
-            "adc2", LazyMp(testdata_cache.refstate("h2o_sto3g", case="gen"))
-        )
-
         # Solve for singlets
-        guesses = adcc.guesses_singlet(matrix, n_guesses=9, block="ph")
-        res = jacobi_davidson(matrix, guesses, n_ep=9)
+        guesses = adcc.guesses_singlet(self.matrix, n_guesses=9, block="ph")
+        res = jacobi_davidson(self.matrix, guesses, n_ep=9)
 
         ref_singlets = refdata["eigenvalues"]
         n_states = min(len(ref_singlets), len(res.eigenvalues))
@@ -54,13 +115,10 @@ class TestSolverDavidson(unittest.TestCase):
         refdata = testdata_cache.adcman_data(
             system="h2o_sto3g", method="adc2", case="gen"
         )["triplet"]
-        matrix = adcc.AdcMatrix(
-            "adc2", LazyMp(testdata_cache.refstate("h2o_sto3g", case="gen"))
-        )
 
         # Solve for triplets
-        guesses = adcc.guesses_triplet(matrix, n_guesses=10, block="ph")
-        res = jacobi_davidson(matrix, guesses, n_ep=10)
+        guesses = adcc.guesses_triplet(self.matrix, n_guesses=10, block="ph")
+        res = jacobi_davidson(self.matrix, guesses, n_ep=10)
 
         ref_triplets = refdata["eigenvalues"]
         n_states = min(len(ref_triplets), len(res.eigenvalues))
