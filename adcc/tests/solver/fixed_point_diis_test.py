deeb36c23b1e0be4f65fb5f1ba25e8da7edd2cf8
Jonas Leitner <jonas.leitner1@gmx.de>
2026-01-19 15:25:11 +0100

    remove random test

--- adcc/tests/solver/fixed_point_diis_test.py
+++ adcc/tests/solver/fixed_point_diis_test.py
@@ -120,31 +120,6 @@ class TestDIIS:
         np_matrix = matrix.to_ndarray()
         ref_eigvals, _ = np.linalg.eigh(np_matrix)
         assert pytest.approx(omega) == ref_eigvals[0]
-
-    def test_adc1_random(self):
-        # try to converge the lowest excited state
-        conv_tol = 1e-9
-        matrix = AdcMatrix(
-            "adc1", testdata_cache.refstate("h2o_sto3g", case="gen")
-        )
-        # I guess with a random guess it is VERY likely that we converge onto
-        # the lowest state, but not guaranteed if the random guess is very
-        # close to another eigenstate -> only verify convergence in this test
-        guess = guess_zero(matrix)
-        guess.set_random()
-        guess /= np.sqrt(guess.dot(guess))
-        solution = diis(updater=self.adc_updater(matrix), guess_vector=guess,
-                        conv_tol=conv_tol,
-                        callback=default_print)
-        # ensure the solution is normalized
-        assert pytest.approx(np.sqrt(solution @ solution)) == 1
-        # ensure that we indeed converged by computing the residual:
-        # MY - wY = 0
-        mvp = matrix @ solution
-        omega = solution.dot(mvp)
-        residual = mvp - omega * solution
-        assert isinstance(residual, AmplitudeVector)
-        assert np.sqrt(residual.dot(residual)) < conv_tol
         # if we now restart the diis with the converged solution we should
         # converge instantly (after performing a single linear step)
         other_solution = diis(
