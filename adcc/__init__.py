30dcb834638aaad2347355de2261e14988ed4cf6
Jonas Leitner <jonas.leitner1@gmx.de>
2026-04-09 09:24:56 +0200

    Bump version: 0.16.1 → 0.17.0

--- adcc/__init__.py
+++ adcc/__init__.py
@@ -70,7 +70,7 @@ __all__ = ["run_adc", "InputError", "AdcMatrix",
            "cvs_adc0", "cvs_adc1", "cvs_adc2", "cvs_adc2x", "cvs_adc3",
            "banner"]
 
-__version__ = "0.16.1"
+__version__ = "0.17.0"
 __license__ = "GPL v3"
 __url__ = "https://adc-connect.org"
 __authors__ = ["Michael F. Herbst", "Maximilian Scheurer", "Jonas Leitner",
