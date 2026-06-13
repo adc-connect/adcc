0a798857b4ce9296457160ef27cf4b8d57539673
Jonas Leitner <jonas.leitner1@gmx.de>
2026-06-13 14:02:54 +0200

    update testdata

--- adcc/tests/data/update_testdata.sh
+++ adcc/tests/data/update_testdata.sh
@@ -4,7 +4,7 @@
 trap "exit 1" TERM
 export SCRIPT_PID=$$
 
-SOURCE="https://wwwagdreuw.iwr.uni-heidelberg.de/adcc_test_data/0.7.4/"
+SOURCE="https://wwwagdreuw.iwr.uni-heidelberg.de/adcc_test_data/groundstate/"
 
 SHAFILE="SHA256SUMS"
 
